8b87ee7386f20c1bd0f57ed7e14bd6079f3c0ee3
view kornkamon <kornkamon.lert@gmail.com>
2024-07-28 00:48:31 +0700

    fix: Edit vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // ระบุชื่อ directory ที่ Vite จะ output ผลลัพธ์การ build
-    sourcemap: true, // เปิดการสร้าง sourcemaps สำหรับการดีบัก
-  },
-  server: {
-    port: 3000, // ระบุพอร์ตที่เซิร์ฟเวอร์การพัฒนาจะรัน
-    strictPort: true, // หากเปิด true เซิร์ฟเวอร์จะไม่รันถ้าพอร์ตถูกใช้
-    proxy: {
-      "/api": "http://localhost:4001", // กำหนด proxy สำหรับ API requests หาก backend รันที่พอร์ตนี้
-    },
-  },
 });
